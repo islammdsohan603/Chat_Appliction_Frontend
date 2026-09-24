50359e642817e76d0f13fd20027b0a850e52877c
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-24 09:08:44 +0600

    shadcn ui install

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
+  envPrefix: ["VITE_", "NEXT_PUBLIC_"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
